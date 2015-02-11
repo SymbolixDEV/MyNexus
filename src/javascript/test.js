2ae6d8913e1486a7934523b1785ed4ecaf51e021
SeanCorcoran <sean.corcoran15@gmail.com>
2015-02-10 21:14:10 -0800

    Design doc updates and apiKey removal

--- src/javascript/test.js
+++ src/javascript/test.js
@@ -1,8 +1,7 @@
-var apiKey = "api_key=34b3ac69-7cb3-4c45-9238-9f88195f3312";
-
+//var apiKey = ""
 //API URLS
 // Champ by ID (apiChamp.concat(ID).concat("?").concat(apiKey))
-var apiChamp = "https://na.api.pvp.net/api/lol/na/v1.2/champion/"
+var apiChamp = "https://na.api.pvp.net/api/lol/na/v1.2/champion/";
 
 //Function for handing HTTP Requests
 function httpGet(apiUrl)
@@ -22,6 +21,6 @@ var champJSON = httpGet(apiChamp.concat(champID).concat("?").concat(apiKey))
 var champObject = JSON.parse(champJSON);
 
 (function() {
-  console.log(champJSON)
+  console.log(champJSON);
   alert(champObject.id);
 })()
